a90106839a1605c2d71952795cb32bd0260afb99
Ari Gesher <ari@gesher.net>
2022-07-27 17:39:29 -0700

    Add in data logger

--- teacup/__main__.py
+++ teacup/__main__.py
@@ -5,17 +5,31 @@ from datetime import datetime
 from io import StringIO
 import json
 import logging
+import pathlib
 from socket import AF_INET
 from typing import Any
 
 from .tempest.messages import get_message_object
 
-LOGGING_CONFIG = {}
+LOGFILE_NAME = "teacup-data.log"
 
+def _configure_data_log():
+    log = logging.getLogger("datalog")
+    handler = logging.FileHandler(str(pathlib.Path(LOGFILE_NAME)))
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter(fmt="%(message)s"))
+    log.addHandler(handler)
+    log.propagate = False 
+    log.setLevel(logging.INFO)
+    return log 
 
 
 class TempestProtocol(DatagramProtocol):
     log = logging.getLogger("tempest.udp")
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.data_log = _configure_data_log()
 
     def datagram_received(self, data: bytes, addr) -> None:
         try:
@@ -33,13 +47,16 @@ class TempestProtocol(DatagramProtocol):
                 data = asdict(event_object)
                 data.update({"type": event_object.__class__.__name__,
                             "src": addr[0]})
-                print(json.dumps(data, default=lambda _: int(_.timestamp())))
+                msg = json.dumps(data, default=lambda _: int(_.timestamp()))
+                print(msg)
+                self.data_log.info(msg)
             else:
                 print("ERROR got non dataclass response: {}".format(event_object))
 
 
 def main():
     logging.basicConfig(level=logging.WARNING)
+    print("Data will be logged to {}".format(LOGFILE_NAME))
     loop = asyncio.new_event_loop()
     async def init():
         log = logging.getLogger("main.async-init")
